19b4e902e6275cc6edccad260c7c8e6d351d3f1f
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-03-29 23:14:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	for (let i = 0; i < s1.length; i++) {
+		if (s1[i]==s2[i]) {
+			console.log(i)
+			break;
+		}
+		
+	}
 }
 
-// Please do not change the code below
+ Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
