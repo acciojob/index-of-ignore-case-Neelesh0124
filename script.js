387ac7a09aad6bde4bd6d5d8cc7bb5f8e6af3cd4
Neelesh0124 <142391511+Neelesh0124@users.noreply.github.com>
2024-03-29 23:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function indexOfIgnoreCase(s1, s2) {
 			console.log(i)
 			break;
 		}
-		
+	
 	}
 }
 
- Please do not change the code below
+ //Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
